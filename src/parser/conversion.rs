f0625ccf7306f6f4e1e7f9a4678d8847e023f3e7
utiyama <ninjin0604@gmail.com>
2022-09-15 00:42:59 +0900

    base64クレートの導入

--- src/parser/conversion.rs
+++ src/parser/conversion.rs
@@ -1,11 +1,13 @@
 use super::emoji_pattern::emoji_pattern;
+use base64;
 
 pub struct Conversion {}
 
 impl Conversion {
     pub fn to_emoji(text: String) -> String {
         let pattern = emoji_pattern();
-        let mut str = text;
+        let mut str = base64::encode(text);
+        println!("{}", str);
         for (key, value) in pattern {
             str = str.split(&key).collect::<Vec<&str>>().join(&value)
         }
@@ -15,11 +17,26 @@ impl Conversion {
 
     pub fn to_base64(text: String) -> String {
         let pattern = emoji_pattern();
-        let mut str = text;
+        let mut str: String = text;
         for (key, value) in pattern {
             str = str.split(&value).collect::<Vec<&str>>().join(&key)
         }
 
-        str
+        let bytes = base64::decode(&str).unwrap();
+        let content = String::from_utf8(bytes.to_vec()).unwrap();
+
+        content
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::conversion::Conversion;
+
+    #[test]
+    fn exploration() {
+        let text = "こんにちは、世界".to_string();
+        assert_eq!(Conversion::to_base64(Conversion::to_emoji(text.clone())), text);
     }
 }
